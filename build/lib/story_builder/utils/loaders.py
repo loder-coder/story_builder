012181b3ff4726da122f2288cdd2c279834a0403
DESKTOP-D6L2MSB\user <syinfo4@gmail.com>
2026-03-04 11:26:44 +0900

    test 6

--- build/lib/story_builder/utils/loaders.py
+++
@@ -1,8 +0,0 @@
-import json
-from story_builder.core.graph import StoryGraph
-
-def load_graph(path: str) -> StoryGraph:
-    """Utility to load a StoryGraph from a JSON file."""
-    with open(path, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-    return StoryGraph(**data)
